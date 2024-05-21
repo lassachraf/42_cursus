f1ad943bc9f8020b1942e8b5a10c8aae25b17c53
alassiqu <alassiqu@e1r8p9.1337.ma>
2024-05-21 17:19:13 +0100

    A little update in syntax (PARENTHESIS CASE) !

--- 42_minishell/syntax/parenthesis_utils.c
+++ 42_minishell/syntax/parenthesis_utils.c
@@ -6,7 +6,7 @@
 /*   By: alassiqu <alassiqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 19:40:46 by alassiqu          #+#    #+#             */
-/*   Updated: 2024/05/21 14:38:48 by alassiqu         ###   ########.fr       */
+/*   Updated: 2024/05/21 17:13:32 by alassiqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	is_left_valid3(t_type type)
 
 int	is_right_valid3(t_type type)
 {
-	return (is_pipe_or_and(type) || type == R_PAREN || type == L_PAREN);
+	return (is_pipe_or_and(type) || type == R_PAREN || type == L_PAREN
+		|| type == END);
 }
 
 int	check_right_parenthesis(t_token *token)
@@ -31,10 +32,12 @@ int	check_right_parenthesis(t_token *token)
 	}
 	else if (!token->next || !is_right_valid3(token->next->type))
 	{
-		if (nb_paren() == 0)
-			return (0);
-		else
+		if (!is_right_valid3(token->next->type))
+		{
+			printf(RED "minishell: syntax error near unexpected token `%s'\n" RESET,
+				token->next->value);
 			return (-1);
+		}
 	}
 	return (0);
 }
